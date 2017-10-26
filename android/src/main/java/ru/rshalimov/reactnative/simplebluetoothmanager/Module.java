b24e748a64fd5fff463c628643e3773ce8b5e90b
RobinBobin <r_shalimov@inbox.ru>
2017-10-26 21:45:15 +0300

    onCharacteristicWrite() implemented.

--- android/src/main/java/ru/rshalimov/reactnative/simplebluetoothmanager/Module.java
+++ android/src/main/java/ru/rshalimov/reactnative/simplebluetoothmanager/Module.java
@@ -1,11 +1,13 @@
 package ru.rshalimov.reactnative.simplebluetoothmanager;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.RCTNativeAppEventEmitter;
 
@@ -13,31 +15,20 @@ import android.util.Log;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.UUID;
 
 class Module extends ReactContextBaseJavaModule {
-   private final class BluetoothGattCallback
-      extends android.bluetooth.BluetoothGattCallback
+   private final class BTGattCallback extends BluetoothGattCallback
    {
-      private final Map <Integer, String> eventNames = new HashMap <> ();
-      
-      BluetoothGattCallback() {
-         eventNames.put(Integer.valueOf(BluetoothProfile.
-            STATE_CONNECTED), Module.CONNECTION_STATE_CONNECTED);
-         
-         eventNames.put(Integer.valueOf(BluetoothProfile.
-            STATE_CONNECTING), Module.CONNECTION_STATE_CONNECTING);
-         
-         eventNames.put(Integer.valueOf(BluetoothProfile.
-            STATE_DISCONNECTED), Module.CONNECTION_STATE_DISCONNECTED);
-         
-         eventNames.put(Integer.valueOf(BluetoothProfile.
-            STATE_DISCONNECTING), Module.CONNECTION_STATE_DISCONNECTING);
-      }
-      
       @Override
       public void onConnectionStateChange(
          BluetoothGatt gatt,
@@ -46,45 +37,78 @@ class Module extends ReactContextBaseJavaModule {
       {
          super.onConnectionStateChange(gatt, status, newState);
          
-         final String address = gatt.getDevice().getAddress();
-         
          Log.d(Module.TAG, String.format(
             "onConnectionStateChange('%s', status=%d, newState=%d)",
-               address, status, newState));
+               gatt.getDevice().getAddress(), status, newState));
          
-         final String eventName = eventNames.get(newState);
+         final String eventName =
+            newState == BluetoothProfile.STATE_CONNECTED ? CONNECTED :
+            newState == BluetoothProfile.STATE_CONNECTING ? CONNECTING :
+            newState == BluetoothProfile.STATE_DISCONNECTED ? DISCONNECTED :
+            newState == BluetoothProfile.STATE_DISCONNECTING ? DISCONNECTING :
+            null;
          
          if (eventName != null) {
-            final WritableMap params = Arguments.createMap();
-            
-            params.putBoolean("error", status != BluetoothGatt.GATT_SUCCESS);
-            params.putString("id", address);
-            params.putInt("status", status);
-            
-            emit(eventName, params);
+            emit(eventName, putCommonParams(eventName, gatt, status));
          }
+      }
+      
+      @Override
+      public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+         super.onServicesDiscovered(gatt, status);
+         
+         Log.d(Module.TAG, String.format("onServicesDiscovered('%s', %d)",
+            gatt.getDevice().getAddress(), status));
+         
+         emitServices(gatt, status);
+      }
+      
+      @Override
+      public void onCharacteristicRead(
+         BluetoothGatt gatt,
+         BluetoothGattCharacteristic ch,
+         int status)
+      {
+         super.onCharacteristicRead(gatt, ch, status);
+         
+         Log.d(TAG, String.format("onCharacteristicRead() not implemented yet"));
+      }
+      
+      @Override
+      public void onCharacteristicWrite(
+         BluetoothGatt gatt,
+         BluetoothGattCharacteristic ch,
+         int status)
+      {
+         super.onCharacteristicWrite(gatt, ch, status);
+         
+         final String serviceUuid = ch.getService().getUuid().toString();
+         
+         Log.d(TAG, String.format("onCharacteristicWrite(%s, %s, %s, %d)",
+            gatt.getDevice().getAddress(), serviceUuid, ch.getUuid(), status));
+         
+         final WritableMap params = putCommonParams(
+            CHARACTERISTIC_WRITTEN, gatt, status);
+         
+         params.putString("serviceUuid", serviceUuid);
+         params.putString("characteristicUuid", ch.getUuid().toString());
+         
+         emit(CHARACTERISTIC_WRITTEN, params);
       }
    }
    
-   static final String TAG = "SimpleBluetoothManager";
-   static final String E_UNSPECIFIED = "E_UNSPECIFIED";
+   static final String
+      TAG = "SimpleBluetoothManager",
+      CONNECTED = "CONNECTED",
+      CONNECTING = "CONNECTING",
+      DISCONNECTED = "DISCONNECTED",
+      DISCONNECTING = "DISCONNECTING",
+      SERVICES_DISCOVERED = "SERVICES_DISCOVERED",
+      CHARACTERISTIC_READ = "CHARACTERISTIC_READ",
+      CHARACTERISTIC_WRITTEN = "CHARACTERISTIC_WRITTEN";
    
-   static final String CONNECTION_STATE_CONNECTED;
-   static final String CONNECTION_STATE_CONNECTING;
-   static final String CONNECTION_STATE_DISCONNECTED;
-   static final String CONNECTION_STATE_DISCONNECTING;
-   
-   private final BluetoothGattCallback
-      bluetoothGattCallback = new BluetoothGattCallback();
-   
-   private final Map <String, GattCache> gattCache = new HashMap <> ();
-   
-   static {
-      CONNECTION_STATE_CONNECTED = TAG + "CONNECTION_STATE_CONNECTED";
-      CONNECTION_STATE_CONNECTING = TAG + "CONNECTION_STATE_CONNECTING";
-      CONNECTION_STATE_DISCONNECTED = TAG + "CONNECTION_STATE_DISCONNECTED";
-      CONNECTION_STATE_DISCONNECTING = TAG + "CONNECTION_STATE_DISCONNECTING";
-   }
+   private final Map <String, BluetoothGatt> gatts = new HashMap <> ();
+   private final BTGattCallback btGattCallback = new BTGattCallback();
    
    Module(ReactApplicationContext reactContext) {
       super(reactContext);
@@ -98,76 +122,276 @@ class Module extends ReactContextBaseJavaModule {
    @Override
    public Map <String, Object> getConstants() {
       final Map <String, Object> constants = new HashMap <> ();
+      final WritableMap events = Arguments.createMap();
       
-      constants.put("CONNECTION_STATE_CONNECTED", CONNECTION_STATE_CONNECTED);
-      constants.put("CONNECTION_STATE_CONNECTING", CONNECTION_STATE_CONNECTING);
-      constants.put("CONNECTION_STATE_DISCONNECTED", CONNECTION_STATE_DISCONNECTED);
+      for (String [] data : new String [][] {
+         {
+            "connectionState",
+            CONNECTED,
+            CONNECTING,
+            DISCONNECTED,
+            DISCONNECTING
+         }, {
+            "gatt",
+            SERVICES_DISCOVERED,
+            CHARACTERISTIC_READ,
+            CHARACTERISTIC_WRITTEN
+         }
+      }) {
+         final WritableMap map = Arguments.createMap();
+         
+         for (int index = 1; index < data.length; index++) {
+            map.putString(data[index], data[index]);
+         }
+         
+         events.putMap(data[0], map);
+      }
       
-      constants.put("CONNECTION_STATE_DISCONNECTING",
-         CONNECTION_STATE_DISCONNECTING);
+      constants.put("events", events);
       
       return constants;
    }
    
    @ReactMethod
-   public void checkBluetoothAddress(String address, Promise promise) {
+   public void isValid(String address, Promise promise) {
       final String addr = address.toUpperCase();
       
       if (BluetoothAdapter.checkBluetoothAddress(addr)) {
          promise.resolve(null);
       } else {
-         promise.reject(E_UNSPECIFIED, String.format(
+         promise.reject("", String.format(
             "Invalid device id: '%s'", addr));
       }
    }
    
    @ReactMethod
-   public void connectGatt(String address, Boolean autoConnect, Callback error) {
-      Log.d(TAG, String.format("connectGatt(%s, %s)", address, autoConnect));
-      
+   public void isEnabled(Promise promise) {
+      promise.resolve(BluetoothAdapter.getDefaultAdapter().isEnabled());
+   }
+   
+   @ReactMethod
+   public void connectGatt(String address, Boolean autoConnect, Promise promise) {
       final String addr = address.toUpperCase();
       
-      if (!BluetoothAdapter.checkBluetoothAddress(addr)) {
-         error.invoke(String.format("Invalid device id: '%s'", addr));
-      } else if (!gattCache.containsKey(addr)) {
-         gattCache.put(addr, new GattCache(BluetoothAdapter.getDefaultAdapter().
-            getRemoteDevice(addr).connectGatt(getReactApplicationContext(),
-               autoConnect, bluetoothGattCallback)));
+      try {
+         if (!BluetoothAdapter.checkBluetoothAddress(addr)) {
+            throw new IllegalArgumentException(String.
+               format("Invalid device id: '%s'", addr));
+         }
+         
+         final BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+         
+         if (!adapter.isEnabled()) {
+            throw new IllegalStateException("Bluetooth is not enabled");
+         }
+         
+         final boolean connect = !gatts.containsKey(addr);
+         
+         if (connect) {
+            gatts.put(addr, adapter.getRemoteDevice(addr).connectGatt(
+               getReactApplicationContext(), autoConnect, btGattCallback));
+         }
+         
+         Log.d(TAG, String.format("connectGatt('%s', %s), %s",
+            addr, autoConnect, connect));
+         
+         promise.resolve(null);
+      } catch (IllegalArgumentException | IllegalStateException e) {
+         promise.reject("", e.getMessage());
       }
    }
    
    @ReactMethod
-   public void closeGatt(String address, Callback error) {
-      Log.d(TAG, String.format("closeGatt(%s)", address));
-      
+   public void discoverServices(String address, Boolean useCache, Promise promise) {
       try {
-         final GattCache gatt = getGattCache(address, String.
-            format("GATT for '%s' hasn't been connected.", address));
+         final BluetoothGatt gatt = getGatt(address);
+         final boolean cacheUsed = useCache && !gatt.getServices().isEmpty();
          
-         gatt.gatt.close();
-         
-         if (gattCache.remove(address.toUpperCase()) == null) {
+         if (cacheUsed) {
+            emitServices(gatt, BluetoothGatt.GATT_SUCCESS);
+         } else if (!gatt.discoverServices()) {
             throw new IllegalStateException(String.format(
-               "gattCache.remove('%s') == null", address.toUpperCase()));
+               "BluetoothGatt.discoverServices() failed for '%s'", address));
          }
+         
+         Log.d(TAG, String.format("discoverServices('%s', %s), %s",
+            address, useCache, cacheUsed));
+         
+         promise.resolve(null);
       } catch (IllegalStateException e) {
-         error.invoke(e.getMessage());
+         promise.reject("", e.getMessage());
       }
    }
    
-   private GattCache getGattCache(String address, String errorMessage) {
-      final GattCache gatt = gattCache.get(address.toUpperCase());
+   @ReactMethod
+   public void readCharacteristic(
+      String address,
+      String serviceUuid,
+      String characteristicUuid,
+      ReadableMap options,
+      Promise promise)
+   {
+      promise.reject("", "Not implemented yet");
+   }
+   
+   @ReactMethod
+   public void writeCharacteristic(
+      String address,
+      String serviceUuid,
+      String characteristicUuid,
+      ReadableMap dataAndOptions,
+      Promise promise)
+   {
+      try {
+         final BluetoothGatt gatt = getGatt(address);
+         
+         final BluetoothGattCharacteristic ch = getCharacteristic(
+            gatt, serviceUuid, characteristicUuid);
+         
+         final ReadableArray value = dataAndOptions.getArray("value");
+         final byte [] valueBuffer = new byte[value.size()];
+         
+         for (int index = 0; index < valueBuffer.length; index++) {
+            valueBuffer[index] = (byte)value.getInt(index);
+         }
+         
+         final String logString = String.format("(%s, %s, %s, %s)", address,
+            serviceUuid, characteristicUuid, Arrays.toString(valueBuffer));
+         
+         if (!ch.setValue(valueBuffer)) {
+            throw new IllegalStateException(String.format(
+               "!ch.setValue() for %s", logString));
+         }
+         
+         if (!gatt.writeCharacteristic(ch)) {
+            throw new IllegalStateException(String.format(
+               "!gatt.writeCharacteristic() for %s", logString));
+         }
+         
+         Log.d(TAG, String.format("writeCharacteristic%s", logString));
+         
+         promise.resolve(null);
+      } catch (IllegalStateException | IllegalArgumentException e) {
+         promise.reject("", e.getMessage());
+      }
+   }
+   
+   @ReactMethod
+   public void closeGatt(String address, Promise promise) {
+      try {
+         final BluetoothGatt gatt = getGatt(address);
+         
+         if (gatts.remove(gatt.getDevice().getAddress()) == null) {
+            throw new IllegalStateException(String.format(
+               "gatts.remove('%s') == null", gatt.getDevice().getAddress()));
+         }
+         
+         gatt.close();
+         
+         Log.d(TAG, String.format("closeGatt('%s')", address));
+         
+         promise.resolve(null);
+      } catch (IllegalStateException e) {
+         promise.reject("", e.getMessage());
+      }
+   }
+   
+   private BluetoothGatt getGatt(String address) {
+      final String addr = address.toUpperCase();
+      final BluetoothGatt gatt = gatts.get(addr);
       
       if (gatt == null) {
-         throw new IllegalStateException(errorMessage);
+         throw new IllegalStateException(String.format(
+            "GATT for '%s' hasn't been connected", addr));
       }
       
       return gatt;
+   }
+   
+   private WritableMap putCommonParams(
+      String eventName,
+      BluetoothGatt gatt,
+      int status)
+   {
+      final WritableMap params = Arguments.createMap();
+      
+      params.putString("eventName", eventName);
+      params.putString("id", gatt.getDevice().getAddress());
+      params.putInt("status", status);
+      params.putBoolean("error", status != BluetoothGatt.GATT_SUCCESS);
+      
+      return params;
+   }
+   
+   private void emitServices(BluetoothGatt gatt, int status) {
+      final WritableArray services = Arguments.createArray();
+      
+      for (BluetoothGattService service : gatt.getServices()) {
+         final WritableArray chars = Arguments.createArray();
+         
+         for (BluetoothGattCharacteristic ch : service.getCharacteristics()) {
+            final WritableMap c = Arguments.createMap();
+            
+            c.putString("uuid", ch.getUuid().toString());
+            c.putInt("instanceId", ch.getInstanceId());
+            c.putInt("permissions", ch.getPermissions());
+            c.putInt("properties", ch.getProperties());
+            c.putInt("writeType", ch.getWriteType());
+            
+            chars.pushMap(c);
+         }
+         
+         final WritableMap srvc = Arguments.createMap();
+         
+         srvc.putString("uuid", service.getUuid().toString());
+         srvc.putInt("instanceId", service.getInstanceId());
+         srvc.putArray("characteristics", chars);
+         
+         services.pushMap(srvc);
+      }
+      
+      final WritableMap params = putCommonParams(SERVICES_DISCOVERED, gatt, status);
+      
+      params.putArray("services", services);
+      
+      emit(SERVICES_DISCOVERED, params);
    }
    
    private void emit(String eventName, WritableMap params) {
       getReactApplicationContext()
          .getJSModule(RCTNativeAppEventEmitter.class)
          .emit(eventName, params);
+   }
+   
+   private BluetoothGattCharacteristic getCharacteristic(
+      BluetoothGatt gatt,
+      String serviceUuid,
+      String characteristicUuid)
+   {
+      final String address = gatt.getDevice().getAddress();
+      
+      if (gatt.getServices().isEmpty()) {
+         throw new IllegalStateException(String.format(
+            "Services haven't been discovered yet for '%s'", address));
+      }
+      
+      final BluetoothGattService service = gatt.
+         getService(UUID.fromString(serviceUuid));
+      
+      if (service == null) {
+         throw new IllegalArgumentException(String.format(
+            "'%s' has no service with uuid '%s'", address, serviceUuid));
+      }
+      
+      final BluetoothGattCharacteristic ch = service.
+         getCharacteristic(UUID.fromString(characteristicUuid));
+      
+      if (ch == null) {
+         throw new IllegalArgumentException(String.format(
+            "Service '%s' of '%s' has no characteristic with uuid '%s'",
+               serviceUuid, address, characteristicUuid));
+      }
+      
+      return ch;
    }
 }
--- index.js
+++ index.js
@@ -7,38 +7,121 @@ const bt = NativeModules.SimpleBluetoothManager;
 const emitter = new NativeEventEmitter(bt);
 
 export default class BluetoothDevice {
-   static CONNECTION_STATE_CONNECTED = bt.CONNECTION_STATE_CONNECTED;
-   static CONNECTION_STATE_CONNECTING = bt.CONNECTION_STATE_CONNECTING;
-   static CONNECTION_STATE_DISCONNECTED = bt.CONNECTION_STATE_DISCONNECTED;
-   static CONNECTION_STATE_DISCONNECTING = bt.CONNECTION_STATE_DISCONNECTING;
+   static events = bt.events;
    
-   constructor(id) {
+   constructor(
+      id,
+      connected,
+      disconnected,
+      servicesDiscovered,
+      characteristicRead,
+      characteristicWritten,
+      characteristicChanged)
+   {
       this.id = id || "";
       this.listeners = [];
-   }
-   
-   isValid() {
-      return bt.checkBluetoothAddress(id);
+      this.readRequests = [];
+      this.writeRequests = [];
+      
+      for (data of [
+         [BluetoothDevice.events.connectionState.CONNECTED, connected],
+         [BluetoothDevice.events.connectionState.DISCONNECTED, disconnected],
+         [BluetoothDevice.events.gatt.SERVICES_DISCOVERED, servicesDiscovered],
+         [BluetoothDevice.events.gatt.CHARACTERISTIC_READ,
+            characteristicRead, this._characteristicReadWritten],
+         [BluetoothDevice.events.gatt.CHARACTERISTIC_WRITTEN,
+            characteristicWritten, this._characteristicReadWritten]
+      ]) {
+         for (let index = 1; index < data.length; index++) {
+            if (data[index]) {
+               this.addListener(data[0], data[index]);
+            }
+         }
+      }
    }
    
    addListener(eventType, listener) {
-      this.listeners.push(emitter.addListener(eventType, listener))
-   }
-   
-   addConnectedDisconnectedListeners(connected, disconnected) {
-      this.addListener(BluetoothDevice.CONNECTION_STATE_CONNECTED, connected);
-      this.addListener(BluetoothDevice.CONNECTION_STATE_DISCONNECTED, disconnected);
+      // console.log(`addListener(): ${eventType}`);
+      this.listeners.push(emitter.addListener(eventType, listener));
    }
    
    removeAllListeners() {
       this.listeners.forEach(listener => listener.remove());
    }
    
-   connectGatt(autoConnect = true, error = console.log) {
-      bt.connectGatt(this.id, autoConnect, error);
+   isValid() {
+      return bt.isValid(id);
    }
    
-   closeGatt(error = console.log) {
-      bt.closeGatt(this.id, error);
+   isEnabled() {
+      return bt.isEnabled();
+   }
+   
+   connectGatt(autoConnect = true) {
+      return bt.connectGatt(this.id, autoConnect);
+   }
+   
+   discoverServices(useCache = true) {
+      return bt.discoverServices(this.id, useCache);
+   }
+   
+   readCharacteristic(serviceUuid, characteristicUuid, options) {
+      return this._safeReadWrite(
+         true, {
+            serviceUuid,
+            characteristicUuid,
+            obj: options
+         });
+   }
+   
+   writeCharacteristic(serviceUuid, characteristicUuid, dataAndOptions) {
+      return this._safeReadWrite(
+         false, {
+            serviceUuid,
+            characteristicUuid,
+            obj: dataAndOptions
+         });
+   }
+   
+   closeGatt() {
+      return bt.closeGatt(this.id);
+   }
+   
+   async _safeReadWrite(read, request) {
+      const operation = read ? "read" : "write";
+      const requests = this[operation + "Requests"];
+      
+      const result = requests.length ? undefined :
+         await bt[operation + "Characteristic"](
+            this.id,
+            request.serviceUuid,
+            request.characteristicUuid,
+            request.obj);
+      
+      requests.push(request);
+      
+      return result;
+   }
+   
+   _characteristicReadWritten({id, eventName}) {
+      if (id.valueOf() == this.id) {
+         const read = eventName.valueOf() == bt.events.gatt.CHARACTERISTIC_READ;
+         const operation = read ? "read" : "write";
+         const requests = this[operation + "Requests"];
+         
+         requests.shift();
+         
+         if (requests.length) {
+            bt[operation + "Characteristic"](
+               this.id,
+               requests[0].serviceUuid,
+               requests[0].characteristicUuid,
+               requests[0].obj).catch(error => {
+                  console.log(error);
+                  
+                  this._characteristicReadWritten(id, eventName);
+               });
+         }
+      }
    }
 }
