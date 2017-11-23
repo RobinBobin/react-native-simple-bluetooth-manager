ed971eccc0fc95fd5647f1741221c948e3db9c21
Robin Shalimov <r_shalimov@inbox.ru>
2017-11-23 15:49:53 +0300

    iOS 2

--- ios/SimpleBluetoothManager.m
+++ ios/SimpleBluetoothManager.m
@@ -1,13 +1,66 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTEventEmitter.h>
 
-#import "SimpleBluetoothManager.h"
+@interface RCT_EXTERN_MODULE(SimpleBluetoothManager, RCTEventEmitter)
 
-@implementation SimpleBluetoothManager
+RCT_EXTERN_METHOD(isValid:
+  (NSString *)address
+  resolver: (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+RCT_EXTERN_METHOD(isEnabled:
+  (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
+
+RCT_EXTERN_METHOD(connectGatt:
+  (NSString *)address
+  autoConnect: (BOOL)autoConnect
+  resolver: (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
+
+RCT_EXTERN_METHOD(discoverServices:
+  (NSString *)address
+  useCache: (BOOL)useCache
+  resolver: (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
+
+RCT_EXTERN_METHOD(readCharacteristic:
+  (NSString *)address
+  serviceUuid: (NSString *)serviceUuid
+  characteristicUuid: (NSString *)characteristicUuid
+  options: (NSDictionary *)options
+  resolver: (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
+
+RCT_EXTERN_METHOD(writeCharacteristic:
+  (NSString *)address
+  serviceUuid: (NSString *)serviceUuid
+  characteristicUuid: (NSString *)characteristicUuid
+  options: (NSDictionary *)dataAndOptions
+  resolver: (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
+
+RCT_EXTERN_METHOD(closeGatt:
+  (NSString *)address
+  resolver: (RCTPromiseResolveBlock)resolver
+  rejecter: (RCTPromiseRejectBlock)rejecter)
+
+- (NSDictionary *)constantsToExport {
+  return @{
+    @"events": @{
+      @"connectionState": @{
+        @"CONNECTED": @"CONNECTED",
+        @"CONNECTING": @"CONNECTING",
+        @"DISCONNECTED": @"DISCONNECTED",
+        @"DISCONNECTING": @"DISCONNECTING"
+      },
+      @"gatt": @{
+        @"SERVICES_DISCOVERED": @"SERVICES_DISCOVERED",
+        @"CHARACTERISTIC_READ": @"CHARACTERISTIC_READ",
+        @"CHARACTERISTIC_WRITTEN": @"CHARACTERISTIC_WRITTEN"
+      }
+    }
+  };
 }
-RCT_EXPORT_MODULE()
 
 @end
-  
---
+++ ios/SimpleBluetoothManager.swift
@@ -0,0 +1,79 @@
+//
+//  SimpleBluetoothManager.swift
+//  SimpleBluetoothManager
+//
+//  Created by Robin Shalimov on 22/11/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+import Foundation
+
+@objc(SimpleBluetoothManager) class SimpleBluetoothManager: RCTEventEmitter {
+  override func supportedEvents() -> [String]! {
+    return [
+      "CONNECTED",
+      "CONNECTING",
+      "DISCONNECTED",
+      "DISCONNECTING",
+      "SERVICES_DISCOVERED",
+      "CHARACTERISTIC_READ",
+      "CHARACTERISTIC_WRITTEN"
+    ];
+  }
+  
+  @objc func isValid(_ address: String, resolver: RCTPromiseResolveBlock, rejecter: RCTPromiseRejectBlock) -> Void {
+    resolver(nil);
+  }
+
+  @objc func isEnabled(_ resolver: RCTPromiseResolveBlock, rejecter: RCTPromiseRejectBlock) -> Void {
+    resolver(true);
+  }
+  
+  @objc func connectGatt(
+    _ address: String,
+    autoConnect: Bool,
+    resolver: RCTPromiseResolveBlock,
+    rejecter: RCTPromiseRejectBlock) -> Void
+  {
+    rejecter("", "can't connect yet", nil);
+  }
+  
+  @objc func discoverServices(
+    _ address: String,
+    useCache: Bool,
+    resolver: RCTPromiseResolveBlock,
+    rejecter: RCTPromiseRejectBlock) -> Void
+  {
+    rejecter("", "can't discover yet", nil);
+  }
+  
+  @objc func readCharacteristic(
+    _ address: String,
+    serviceUuid: String,
+    characteristicUuid: String,
+    options: [String: Any],
+    resolver: RCTPromiseResolveBlock,
+    rejecter: RCTPromiseRejectBlock)
+  {
+    rejecter("", "can't read yet", nil);
+  }
+  
+  @objc func writeCharacteristic(
+    _ address: String,
+    serviceUuid: String,
+    characteristicUuid: String,
+    dataAndOptions: [String: Any],
+    resolver: RCTPromiseResolveBlock,
+    rejecter: RCTPromiseRejectBlock)
+  {
+    rejecter("", "can't write yet", nil);
+  }
+  
+  @objc func closeGatt(
+    _ address: String,
+    resolver: RCTPromiseResolveBlock,
+    rejecter: RCTPromiseRejectBlock)
+  {
+    rejecter("", "can't close yet", nil);
+  }
+}
